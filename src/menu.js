57b989295cb12b323f6a056971494355d5db458d
PiratarrRn <linneathebeast@hotmail.com>
2018-11-14 14:51:58 +0100

    fart

--- src/menu.js
+++ src/menu.js
@@ -4,14 +4,14 @@ export default class menuState extends Phaser.State {
         {font: '50px Arial', fill: '#ffffff'});
 
     const startLabel = this.game.add.text(80, this.game.world.height-80,
-        'press "w" to start',
-        {font: '25px Arial', fill: '#ffffff'});
+        'press "w" to fart',
+        {font: '25px Arifal', fill: '#ffffff'});
     
     const wkey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
     wkey.onDown.addOnce(this.start, this);
   }
 
   start() {
-    this.game.state.start('play');
+    this.game.state.start('slay');
   }
 }
