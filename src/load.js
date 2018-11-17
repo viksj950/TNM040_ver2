938751da1723e7437200225cbd622f0774f71015
PiratarrRn <linneathebeast@hotmail.com>
2018-11-17 16:13:34 +0100

    kukianus

--- src/load.js
+++ src/load.js
@@ -1,10 +1,11 @@
 export default class loadState extends Phaser.State {
   preload() {
     let loadingLabel = this.game.add.text(80, 150, 'loading...',
-        {font: '30px Courier', fill: '#ffffff'});
+        {font: '30px Courier', fill: '#afdfdd'});
 
     this.game.load.image('player', './assets/images/epple.png');
     this.game.load.image('obstacle', './assets/images/boll.png');
+	this.game.load.image('backgroundimage','./assets/images/phaser.png');
   }
 
   create() {
--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,12 @@
 export default class menuState extends Phaser.State {
   create() {
     const nameLabel = this.game.add.text(80, 80, 'yo momma so fat this screen is black',
-        {font: '50px Arial', fill: '#ffffff'});
+        {font: '50px Arial', fill: '#fcbfff'});
 
     const startLabel = this.game.add.text(80, this.game.world.height-80,
 
         'press "space" to start',
-        {font: '25px Arial', fill: '#ffffff'});
+        {font: '25px Arial', fill: '#fffeab'});
 
     
     const wkey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
